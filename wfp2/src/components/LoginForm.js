c381794fe4ad7bcba15cfb0df90f0d45f8d80241
Chang Syu <stevenchen38@gmail.com>
2021-05-21 12:49:31 +0800

    Login page

--- wfp2/src/components/LoginForm.js
+++ wfp2/src/components/LoginForm.js
@@ -15,16 +15,16 @@ function LoginForm({Login, error}) {
                 </div>
                 {(error !='')?(<div className='error'>{error}</div>):''}
                 <div className="form-group">
-                    <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" id="name" onChange={e=>setDetails({...details, name:e.target.value})} value={details.name}/>
+                    <label htmlFor="name"></label>
+                    <input type="text" name="name" id="name" placeholder="Name" onChange={e=>setDetails({...details, name:e.target.value})} value={details.name}/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="email">Email: </label>
-                    <input type="text" name="email" id="email" onChange={e=>setDetails({...details, email:e.target.value})} value={details.email}/>
+                    <label htmlFor="email"></label>
+                    <input type="text" name="email" id="email" placeholder="Email" onChange={e=>setDetails({...details, email:e.target.value})} value={details.email}/>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" id="password" onChange={e=>setDetails({...details, password:e.target.value})} value={details.password}/>
+                    <label htmlFor="password"></label>
+                    <input type="password" name="password" id="password" placeholder="Password" onChange={e=>setDetails({...details, password:e.target.value})} value={details.password}/>
                 </div>
                 <input type="submit" value="Login"/>
             </div>
