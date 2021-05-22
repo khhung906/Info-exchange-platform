d2a61f4e83a8f3575196b3b50f4c0c42ef0f03a4
Chang Syu <stevenchen38@gmail.com>
2021-05-22 11:01:16 +0800

    latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     email: "123",
     password:"123"
   }
-  const [user, setUser] = useState({name:'',email:''})
+  const [guest, setGuest] = useState({email:'', password:''})
   const [error, setError] = useState('')
   const [FormType, setType] = useState('login')
 
@@ -15,9 +15,9 @@ function App() {
     console.log(details);
     if (details.email==adminUser.email && details.password==adminUser.password){
       console.log("Logged in")
-      setUser({
-        name: details.name,
-        email: details.email
+      setGuest({
+        email: details.email,
+        password: details.password
       })
     } else {
       console.log("Details do not match.")
@@ -26,12 +26,14 @@ function App() {
   }
 
   const AdminUser = () =>{
-
   }
+
+  // const [user, setUser]= useState=([{name:'', email:'', password:''}])
+
 
   const Logout = () => {
     console.log("Logout")
-    setUser({name:'', email:''})
+    setGuest({email:''})
   }
 
   const FormSwitch = () => {
@@ -46,21 +48,33 @@ function App() {
 
 
   return (
-    <ul>
-      <li className="title">
-          <h1>Info Exchange</h1>
-      </li>
-      <li className="App">
-        {(user.email != "")?(
-          <div>
-            <h2>Welcome, <span>{user.name}</span></h2>
-            <button onClick={Logout}>Logout</button>
-          </div>
-        ):(
-          (FormType === "login")?(<LoginForm Login={Login} error={error} Change={FormSwitch}/>):(
-            <RegisterForm AdminUser={AdminUser} Change={FormSwitch}/>))}
-      </li>
-    </ul>
+    <div>
+      <div className="navBar">
+        <a className="title-name">Info Exchange</a>
+        <a className="redirect">About Us</a>
+        <a className="redirect">Language</a>
+      </div>
+      <div>
+        <ul>
+          <li className="title">
+              <div>
+                <h1 className="title-name">Where you find what you need?</h1>
+                <p className="rights">@2021 NTU All Rigts Reserved.</p>
+              </div>
+          </li>
+          <li className="App">
+            {(guest.email != "")?(
+              <div>
+                <h1>Welcome</h1>
+                <button onClick={Logout}>Logout</button>
+              </div>
+            ):(
+              (FormType === "login")?(<LoginForm Login={Login} error={error} Change={FormSwitch}/>):(
+                <RegisterForm AdminUser={AdminUser} Change={FormSwitch}/>))}
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
