e8f2afbb3bc36a75f641559bb87b5880db1f9a90
johnsonhung906 <khhung906@gmail.com>
2021-05-27 20:23:51 +0800

    simple calendar function

--- frontend/src/components/Calender.js
+++ frontend/src/components/Calender.js
@@ -3,9 +3,12 @@ import Button from '@material-ui/core/Button';
 import MainPageTopBar from './MainPageTopBar';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import AddCourse from './AddCourse';
-import Calendar from 'react-calendar'
 import DeleteCourse from './DeleteCourse';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+const localizer = momentLocalizer(moment);
 
 function Calender() {
     const [open, setOpen] = useState(false);
@@ -18,9 +21,38 @@ function Calender() {
         setOpen(false);
     };
 
-    const [value, onChange] =useState(new Date())
+    const now = new Date();
+    const events = [
+      {
+          id: 0,
+          title: 'All Day Event very long title',
+          allDay: true,
+          start: new Date(2021, 5, 3),
+          end: new Date(2021, 5, 18),
+      },
+      {
+        id: 1,
+        title: 'Long Event',
+        start: new Date(2015, 3, 7),
+        end: new Date(2015, 3, 10),
+      },
+      {
+          id: 2,
+          title: 'DTS STARTS',
+          start: new Date(2016, 2, 13, 0, 0, 0),
+          end: new Date(2016, 2, 20, 0, 0, 0),
+      },
+      {
+        id: 3,
+        title: 'Today',
+        start: now,
+        end: now,
+      },
+    ]
+    const onSelect = (event) =>{
+      console.log(event)
+    }
 
-  
     return (
       <div>
           <MainPageTopBar/>
@@ -32,8 +64,17 @@ function Calender() {
               <Button variant="contained" color="primary" onClick={handleClickOpen}>
                   Delete Course
               </Button>
-          <Calendar onChange={onChange} value={value} className="react-calendar">
-          </Calendar>
+          <div style={{marginLeft:'250pt', marginTop:'50pt', height: '450pt', width:'800pt'}}>
+            <Calendar
+              events={events}
+              views={["month", "week", "day"]}
+              startAccessor="start"
+              endAccessor="end"
+              onSelectEvent={onSelect}
+              defaultDate={moment().toDate()}
+              localizer={localizer}
+            />
+          </div>
       </div>
     );
   }
