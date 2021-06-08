ba5d0d9ecfdc2951a4c1240b3403e17b922dce8f
STV <stv@STVdeMacBook-Pro.local>
2021-06-08 22:29:01 +0800

    map page added

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -1,6 +1,51 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import MainPageTopBar from '../MainPageTopBar';
 import HashLoader from 'react-spinners/HashLoader'
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3R2MTIyMiIsImEiOiJja3BvMzR2ZXI0a3l3MnVueGR0bGxrZjdmIn0.34CD3AHK7plUolQwFSje-Q'
+
+function DefaultMap(){
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(120.4);
+    const [lat, setLat] = useState(23.44);
+    const [zoom, setZoom] = useState(7);
+
+    useEffect(() => {
+        if (map.current) return; //initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        })
+    })
+
+    useEffect(() => {
+        if (!map.current) return; 
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        })
+    })
+    
+    return(
+        <div>
+            <div ref={mapContainer} className="map-container">
+                <div className="map-sidebar">
+                    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+
+
 
 function Map (props) {
     const {userinfo,log_in} = props; 
@@ -27,7 +72,7 @@ function Map (props) {
         return (
             <div>
                 <MainPageTopBar log_in = {log_in}/>
-                
+                <DefaultMap/>
             </div>
         )
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 ReactDOM.render(
   <BrowserRouter>
