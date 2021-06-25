63ff87dd958fe2ee9d1443b37baefef7ddcc5de4
STV <stv@STVdeMacBook-Pro.local>
2021-06-25 20:16:09 +0800

    map comment added

--- frontend/src/components/map/Review.js
+++ frontend/src/components/map/Review.js
@@ -6,11 +6,21 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import LeaveComment from './LeaveComment';
+import Avatar from '@material-ui/core/Avatar';
+import { deepOrange, deepPurple } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
     divider: {
         margin: theme.spacing(3, 0, 2),
-    }
+    },
+    orange: {
+        color: theme.palette.getContrastText(deepOrange[500]),
+        backgroundColor: deepOrange[500],
+      },
+      purple: {
+        color: theme.palette.getContrastText(deepPurple[500]),
+        backgroundColor: deepPurple[500],
+      },
 }));
 
 function Review () {
@@ -24,14 +34,22 @@ function Review () {
                 Reviews
             </Typography>
             <br/>
-            <p style={{fontSize:'15px', fontFamily:'Roboto,Arial,sans-serif'}}>UserName1</p>
-            <br/>
-            <p style={{fontSize:'12px'}}>Very Good</p>
+            <div>
+                <Avatar className={classes.orange}>N</Avatar>
+                <br/>
+                <p style={{fontSize:'15px', fontFamily:'Roboto,Arial,sans-serif'}}>UserName1</p>
+                <br/>
+                <p style={{fontSize:'12px'}}>Very Good</p>
+            </div>
             <Divider variant="middle" className={classes.divider}/>
             <br/>
-            <p style={{fontSize:'15px', fontFamily:'Roboto,Arial,sans-serif'}}>UserName2</p>
-            <br/>
-            <p style={{fontSize:'12px'}}>Very Bad</p>
+            <div>
+                <Avatar className={classes.purple}>H</Avatar>
+                <br/>
+                <p style={{fontSize:'15px', fontFamily:'Roboto,Arial,sans-serif'}}>UserName2</p>
+                <br/>
+                <p style={{fontSize:'12px'}}>Very Bad</p>
+            </div>
             <Divider variant="middle" className={classes.divider}/>
         </div>
     )
