096e536a587fb626364378ec007509626d3029eb
johnsonhung906 <khhung906@gmail.com>
2021-06-27 20:31:21 +0800

    header changes

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_ROOT = "https://info-xchange-server.herokuapp.com/"
+const API_ROOT = "http://localhost:4000"
 
 const instance = axios.create({
   baseURL: API_ROOT,
